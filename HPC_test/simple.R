348aff8ea42dfc04b30d3f2d0dae3d3af4b8c534
Alice Welch <welchali@oregonstate.edu>
2026-05-18 10:13:26 -0700

    HPC stuff
    added hpc files

---
+++ HPC_test/simple.R
@@ -0,0 +1,18 @@
+
+n <- 10
+sqrd <- numeric(length = n)
+
+for (i in 1:n){
+  
+  sqrd[i] <- i^2
+  
+  print( sqrd[i] )
+}
+
+out <- data.frame('Input' = 1:n, 
+                  'Output' = sqrd)
+
+write.csv(out,
+          file = 'MyResults.csv',
+          row.names = FALSE)
+          
